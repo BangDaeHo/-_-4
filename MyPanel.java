e18e45f3e91552ad5912956e4fa75ec12b868cb0
dackyy <eorl4940@naver.com>
2019-12-02 13:24:23 +0900

    TextArea 10 ,24 로 변경

--- MyPanel.java
+++ MyPanel.java
@@ -10,33 +10,33 @@ import java.awt.*;
 public class MyPanel extends JPanel
 {
     private JTextField tf = new JTextField(20);
-    private JTextArea ta = new JTextArea(7, 20);
+    private JTextArea ta = new JTextArea(10, 24);
     private String [] grades = {"1학년", "2학년", "3학년", "4학년"}; 
     private JLabel gradeLabel = new JLabel(grades[0]);
     private JComboBox<String> strCombo = new JComboBox<String> (grades);
     public JPanel mp;
     public JButton btn;
-    
+
     public MyPanel(){
 
         JLabel MyName = new JLabel("이름");
         this.add(MyName);
         JTextField mtfName = new JTextField(20);
         this.add(mtfName);
-        
+
         JLabel MyDept = new JLabel("학과");
         this.add(MyDept);
         JTextField mtfDept = new JTextField("글로벌소프트웨어학과", 20);
         this.add(mtfDept);
-        
+
         JLabel MyAddress = new JLabel("주소");
         this.add(MyAddress);
         JTextField mtfAddress = new JTextField("서울시...", 20);
         this.add(mtfAddress);
-        
+
         JComboBox<String> cbSchoolYear = new JComboBox<String>();
         for(int i=0; i<grades.length; i++) {
-           cbSchoolYear.addItem(grades[i]); // 콤보박스에 문자열 아이템 삽입 
+            cbSchoolYear.addItem(grades[i]); // 콤보박스에 문자열 아이템 삽입 
         }
         this.add(cbSchoolYear);
         JButton mb = new JButton("추가");
@@ -46,7 +46,6 @@ public class MyPanel extends JPanel
         JTextArea Ta = new JTextArea();
         this.add(Ta);
 
-        
         // 텍스트필드에 Enter, 키 입력 때 발생하는 Action 이벤트의 리스너 등록 
         tf.addActionListener(new ActionListener() { 
                 public void actionPerformed(ActionEvent e) { 
@@ -55,8 +54,7 @@ public class MyPanel extends JPanel
                     t.setText(""); // 현재 텍스트필드에 입력된 내용 지우기 
                 }
             });
-        }
+    }
 
 }
-
 
