65e42f093927a6c779860d49aacf8b9073abf8b3
BangDaeHo <hanava1@naber.com>
2019-12-02 13:20:17 +0900

    변수 변경밑 변수 없는 UI에 변수 설정

--- MyPanel.java
+++ MyPanel.java
@@ -7,7 +7,7 @@ import java.awt.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MyPanel extends JFrame
+public class MyPanel extends JPanel
 {
     private JTextField tf = new JTextField(20);
     private JTextArea ta = new JTextArea(7, 20);
@@ -18,28 +18,33 @@ public class MyPanel extends JFrame
     public JButton btn;
     
     public MyPanel(){
-        setTitle("실습_4");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Container c = getContentPane();
-        c.setLayout(new FlowLayout());
 
-        c.add(new JLabel("이름"));
-        c.add(new JTextField(20));
-        c.add(new JLabel("학과"));
-        c.add(new JTextField("글로벌소프트웨어학과", 20));
-        c.add(new JLabel("주소"));
-        c.add(new JTextField("서울시 ...", 20));
+        JLabel MyName = new JLabel("이름");
+        this.add(MyName);
+        JTextField mtfName = new JTextField(20);
+        this.add(mtfName);
         
-        JComboBox<String> gradeCombo = new JComboBox<String>();
+        JLabel MyDept = new JLabel("학과");
+        this.add(MyDept);
+        JTextField mtfDept = new JTextField("글로벌소프트웨어학과", 20);
+        this.add(mtfDept);
+        
+        JLabel MyAddress = new JLabel("주소");
+        this.add(MyAddress);
+        JTextField mtfAddress = new JTextField("서울시...", 20);
+        this.add(mtfAddress);
+        
+        JComboBox<String> cbSchoolYear = new JComboBox<String>();
         for(int i=0; i<grades.length; i++) {
-            gradeCombo.addItem(grades[i]); // 콤보박스에 문자열 아이템 삽입 
+           cbSchoolYear.addItem(grades[i]); // 콤보박스에 문자열 아이템 삽입 
         }
-        c.add(gradeCombo);
-        JButton add = new JButton("추가");
+        this.add(cbSchoolYear);
+        JButton mb = new JButton("추가");
         JButton clear = new JButton("Clear");
-        c.add(add);
-        c.add(clear);
-        c.add(new JScrollPane(ta));
+        this.add(mb);
+        this.add(clear);
+        JTextArea Ta = new JTextArea();
+        this.add(Ta);
 
         
         // 텍스트필드에 Enter, 키 입력 때 발생하는 Action 이벤트의 리스너 등록 
@@ -49,9 +54,18 @@ public class MyPanel extends JFrame
                     ta.append(t.getText() + "\n"); // 텍스트필드의 문자열을 텍스트 영역 창에 추가 
                     t.setText(""); // 현재 텍스트필드에 입력된 내용 지우기 
                 }
-            });
-        setSize (300, 300); 
-        setVisible(true); 
+            });/**
+         * An example of a method - replace this comment with your own
+         *
+         * @param  y  a sample parameter for a method
+         * @return    the sum of x and y
+         */
+        public int sampleMethod(int y)
+        {
+        // put your code here
+        return y;
     }
+
+}
 }
 
